84c4a14952d0eab1c0c65d0ddbd052f999210676
Ajay <ajaypal2961@gmail.com>
2023-03-08 11:13:14 +0530

    added query_execution function

--- dbqueries.py
+++ dbqueries.py
@@ -1,81 +1,54 @@
 from main import engine
-from sqlalchemy import text , bindparam 
+from sqlalchemy import text 
 import datetime as dt
 import click
 import logging
 
-logging.basicConfig(filename='dbquerieslog.log',level=logging.DEBUG,format='%(levelname)s:%(message)s:%(asctime)s')
+logging.basicConfig(filename='dbquerieslog.log',level=logging.DEBUG,format='%(asctime)s - %(levelname)s - %(message)s')
 
-def insert(firstname,lastname,address,dob):
+def insert_assignment1(**params):
 
-    logging.debug(f"firstname :  {firstname} lastname : {lastname} address : {address} dob : {dob}")
-    
     sqlStatement = text("INSERT INTO customer (first_name,last_name,address,dob,created_by,updated_by) VALUES (:first_name,:last_name,:address,:dob,:created_by,:updated_by)")
-    logging.debug(sqlStatement)
+    query_execution(sqlStatement,**params)       
+    logging.info("Data inserted in database")
+
+
+def select_assignment1(**params):
+
+    sqlStatement = text("SELECT * FROM customer WHERE first_name = (:first_name) AND address = (:address)")
+    query_execution(sqlStatement,True,**params)
+    logging.info("Data selected from database")
+
+def update_assingment1(**params):
     
+    sqlStatement = text(f"UPDATE customer SET address = (:address) , updated_by = (:updated_by)  WHERE first_name = (:first_name)")
+    query_execution(sqlStatement,**params)      
+    logging.info("Data updated in database") 
+
+
+def delete_assingment1(**params):
+
+    sqlStatement = text("DELETE FROM customer WHERE first_name = (:first_name) AND address = (:address)")
+    query_execution(sqlStatement,**params)
+    logging.info("Data deleted from database")
+
+def query_execution(sqlStatement , is_select = False , **params):
+    logging.debug(params)
     try :
         with engine.connect() as conn:
-            operation = conn.execute(sqlStatement , [{"first_name": firstname, "last_name": lastname, "address": address, "dob" : dob, "created_by": dt.datetime.now(), "updated_by": dt.datetime.now()}],)
+            records = conn.execute(sqlStatement , [params],)
+            if is_select == True :
+                for eachRecord in records :
+                    print(eachRecord)
             logging.info("sql query executed ! Ready for commit")
             conn.commit()  
     except Exception as e :
-        logging.exception(f"Operation not performed due to {e}")        
-    else :
-        logging.info("Data inserted in database")
+        logging.exception(f"Operation not performed due to {e}")
 
-
-
-def select(firstname , lastname , address , dob):
-
-    logging.debug(f"firstname :  {firstname} lastname : {lastname} address : {address} dob : {dob}")
-    sqlStatement = text("SELECT * FROM customer WHERE first_name = (:first_name)")
-
-    try :
-        with engine.connect() as conn :
-            records = conn.execute(sqlStatement,[{"first_name" : firstname }],)
-            for eachRecord in records :
-                print(eachRecord) 
-    except Exception as e :
-        logging.exception(f"Operation not performed due to {e}")        
-    else :
-        logging.info("Data selected from database")
-
-def update(firstname,lastname,address,dob):
-    
-    logging.debug(f"firstname :  {firstname} lastname : {lastname} address : {address} dob : {dob}")
-    sqlStatement = text("UPDATE customer SET address = (:address) WHERE first_name = (:first_name)")
-
-    try :
-        with engine.connect() as conn :
-            logging.debug(sqlStatement)
-            conn.execute(sqlStatement, [{ "address": address, "first_name": firstname}],)
-            logging.info("sql query executed ! Ready for commit")
-            conn.commit() 
-    except Exception as e :
-        logging.exception(f"Operation not performed due to {e}")        
-    else : 
-        logging.info("Data updated in database") 
-
-
-def delete(firstname,lastname,address,dob):
-
-    logging.debug(f"firstname :  {firstname} lastname : {lastname} address : {address} dob : {dob}")
-    sqlStatement = text("DELETE FROM customer WHERE first_name = (:first_name) AND address = (:address)")
-    
-    try :
-        with engine.connect() as conn :
-            logging.debug(sqlStatement)
-            conn.execute(sqlStatement,[{"first_name" : firstname , "address" : address}],)
-            logging.info("sql query executed ! Ready for commit")
-            conn.commit()
-    except Exception as e :
-        logging.exception(f"Operation not performed due to {e}")        
-    else : 
-        logging.info("Data deletec from database")
 
 
 if __name__ == '__main__':
-    mapping = {"i": insert, "u": update , "s": select, "d": delete}
+    mapping = {"i": insert_assignment1, "u": update_assingment1 , "s": select_assignment1, "d": delete_assingment1}
     
     @click.command()
     @click.option("--toq", default="s")
@@ -83,10 +56,17 @@ if __name__ == '__main__':
     @click.option('-ln','--lastname' ,help = 'last name')
     @click.option('-ad','--address' ,help = 'address')
     @click.option('-d','--dob', type=click.DateTime(formats=["%Y-%m-%d"]),default = str(dt.date.today()))
-    
 
     def runner(toq,firstname,lastname,address,dob):
-        mapping[toq](firstname,lastname,address,dob)
+        dict_variables = {
+            "first_name": firstname, 
+            "last_name": lastname, 
+            "address": address, 
+            "dob" : dob, 
+            "created_by": dt.datetime.now(), 
+            "updated_by": dt.datetime.now()
+        }
+        mapping[toq](**dict_variables)
 
     runner()         
 
--- main.py
+++ main.py
@@ -3,33 +3,27 @@ from sqlalchemy.orm import sessionmaker
 import configparser
 import logging as log
 
-log.basicConfig(filename='dbquerieslog.log',level=log.DEBUG,format='%(levelname)s:%(message)s:%(asctime)s')
+log.basicConfig(filename='dbquerieslog.log',level=log.DEBUG,format='%(asctime)s:%(levelname)s:%(message)s')
 
 try :
     config = configparser.RawConfigParser() 
     config.read('details.properties')
-except Exception as e :
-    log.exception(f'Unable to read file due to {e}')
-
-try : 
     host = config.get('detailsLocal','host') 
     port = config.get('detailsLocal','port')
     databaseName = config.get('detailsDatabase','dataBaseName')
     Username = config.get('detailsDatabase','Username')
     password = config.get('detailsDatabase','password')
-    log.debug(f"host: {host} port: {port} database : {databaseName} Username : {Username} password : {password}")
+    log.debug(f"host: {host} port: {port} database : {databaseName} Username : {Username} password : **")
+
 except Exception as e :
-    log.exception("Unable to fetch values from details.properties due to {e}")
+    log.exception(f'Unable to read file due to {e}')
 
-# Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
+else :    
+    # Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
+    DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(Username,password,host,port,databaseName)
+    log.info(DATABASE_URI)
+    engine = db.create_engine(DATABASE_URI)  # creating engine
+    Session = sessionmaker(bind=engine)      # making global session 
+    s = Session()                            # calling off single session from global Session
+    log.info("Session object called")
 
-DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(Username,password,host,port,databaseName)
-log.info(DATABASE_URI)
-
-engine = db.create_engine(DATABASE_URI)  # creating engine
-
-Session = sessionmaker(bind=engine)      # making global session 
-
-s = Session()                            # calling off single session from global Session
-log.info("Session object called")
-print('Main file completed')
